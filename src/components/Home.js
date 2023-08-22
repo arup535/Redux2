f7cd02cc683cb399537ebf6926aa5dc7940e9d97
arup535 <arupmajumdar535@gmail.com>
2023-08-22 16:04:28 +0530

    fist commit

---
+++ src/components/Home.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Home(){
+
+    return(
+        <div>
+            <div className="add-to-cart">
+                <img src="https://pngimg.com/d/shopping_cart_PNG20.png" ></img>
+            </div>
+            <h1>Home Component</h1>
+            <div className="cart-wrapper">
+                <div className="img-wrapper item">
+                <img className="iphone" src="https://pngimg.com/d/iphone_11_PNG26.png"></img>
+                {/* <img className="iphone" src="https://pngimg.com/d/iphone_11_PNG26.png" style={{width:100, height:150}}></img> */}
+                {/* shopping_cart_PNG28.png */}
+                </div>
+                <div className="txt-wrapper item">
+                    <span style={{display:"flow"}}>i-Phone</span>
+                    <span>Price: $100</span>
+                </div>
+                <div className="btn-wrapper item">
+                    <button>Add To Cart</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Home;
